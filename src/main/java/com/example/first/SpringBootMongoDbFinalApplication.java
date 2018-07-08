541d38d520276801422cd295943dc74482d97d02
nitish24n <nitish24m@gmail.com>
2018-07-08 22:54:37 +0530

    Running spring boot application
    Made using spring boot documentation

---
+++ src/main/java/com/example/first/SpringBootMongoDbFinalApplication.java
@@ -0,0 +1,12 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbFinalApplication.class, args);
+	}
+}
